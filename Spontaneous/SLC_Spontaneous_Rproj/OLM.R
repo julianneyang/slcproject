d5b2352a9367747d97e3b681bd04bac436e618b8
Arjun Gutta <arjun@arjuns-mbp.ad.medctr.ucla.edu>
2022-11-04 15:44:36 -0700

    fixed olm stats

--- Spontaneous/SLC_Spontaneous_Rproj/OLM.R
+++ Spontaneous/SLC_Spontaneous_Rproj/OLM.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(here)
 library(ggplot2)
 library(rlang)
-library(rstatix)  
+library(rstatix)
 library(nlme)
 library(cowplot)
 library(ggbeeswarm)
@@ -29,19 +29,19 @@ data_box_plot$Day <- plyr::revalue(data_box_plot$Day, c('1' = "Training", '2' = 
 #Stratify by Day
 generate_longitudinal_boxplots <- function(input_data, X, Y, min,max){
   data<-as.data.frame(input_data)
-  #Ensure correct ordering of levels 
+  #Ensure correct ordering of levels
   data$Genotype <- factor(data$Genotype, levels = c("WT","HET","MUT"))
   data$Day <- factor(data$Day, levels = c("Training", "Testing"))
   
-  ggplot(data=data,aes(x={{X}},y={{Y}}, fill={{X}})) + 
-    geom_boxplot(alpha=0.25)+ 
+  ggplot(data=data,aes(x={{X}},y={{Y}}, fill={{X}})) +
+    geom_boxplot(alpha=0.25)+
     geom_line(aes(group = MouseID,color=Genotype),size=0.65)+
     scale_fill_viridis_d()+
     geom_point(size=1,position=position_jitter(width=0.25),alpha=0.1)+
     theme_cowplot(16) +
     ggtitle("OLM Analysis - Training v Testing") +
     scale_color_manual(values = c(WT = "#2b8cbe", HET = "#7BCB5B", MUT = "#FB4A4A")) +
-    theme(legend.position = "top") 
+    theme(legend.position = "top")
 }
 
 fullplot<-generate_longitudinal_boxplots(data_box_plot, Day, Percent_Time_Investigated,0,100)
@@ -52,12 +52,12 @@ data_box_plot$Genotype <- factor(data_box_plot$Genotype,levels=c("WT", "HET", "M
 females <- subset(data_box_plot, Sex =="Female")
 generate_longitudinal_boxplots_female <- function(input_data, X, Y, min,max){
   data<-as.data.frame(input_data)
-  #Ensure correct ordering of levels 
+  #Ensure correct ordering of levels
   data$Genotype <- factor(data$Genotype, levels = c("WT","HET","MUT"))
   data$Day <- factor(data$Day, levels = c("Training", "Testing"))
   
-  ggplot(data=females,aes(x={{X}},y={{Y}}, fill={{X}})) + 
-    geom_boxplot(alpha=0.25)+ 
+  ggplot(data=females,aes(x={{X}},y={{Y}}, fill={{X}})) +
+    geom_boxplot(alpha=0.25)+
     geom_line(aes(group = MouseID,color=Genotype),size=0.65)+
     scale_fill_viridis_d()+
     geom_point(size=1,position=position_jitter(width=0.25),alpha=0.1)+
@@ -65,18 +65,18 @@ generate_longitudinal_boxplots_female <- function(input_data, X, Y, min,max){
     ggtitle("OLM Analysis - Females") +
     ylim(10,80) +
     scale_color_manual(values = c(WT = "#2b8cbe", HET = "#7BCB5B", MUT = "#FB4A4A")) +
-    theme(legend.position = "right") 
+    theme(legend.position = "right")
 }
 
 males <- subset(data_box_plot, Sex=="Male")
 generate_longitudinal_boxplots_male <- function(input_data, X, Y, min,max){
   data<-as.data.frame(input_data)
-  #Ensure correct ordering of levels 
+  #Ensure correct ordering of levels
   data$Genotype <- factor(data$Genotype, levels = c("WT","HET","MUT"))
   data$Day <- factor(data$Day, levels = c("Training", "Testing"))
   
-  ggplot(data=males,aes(x={{X}},y={{Y}}, fill={{X}})) + 
-    geom_boxplot(alpha=0.25)+ 
+  ggplot(data=males,aes(x={{X}},y={{Y}}, fill={{X}})) +
+    geom_boxplot(alpha=0.25)+
     geom_line(aes(group = MouseID,color=Genotype),size=0.65)+
     scale_fill_viridis_d()+
     geom_point(size=1,position=position_jitter(width=0.25),alpha=0.1)+
@@ -84,7 +84,7 @@ generate_longitudinal_boxplots_male <- function(input_data, X, Y, min,max){
     ylim(10,80) +
     ggtitle("OLM Analysis - Males") +
     scale_color_manual(values = c(WT = "#2b8cbe", HET = "#7BCB5B", MUT = "#FB4A4A")) +
-    theme(legend.position = "right") 
+    theme(legend.position = "right")
 }
 
 females_only<-generate_longitudinal_boxplots_female(females, Day, Percent_Time_Investigated,0,100)
@@ -116,25 +116,29 @@ plot_grid(violin2, violin3)
 
 #Stat Tests
 # WT vs MUT Training
-data_wm <- data_box_plot %>% filter(Genotype!="HET", Day != "Testing")
+data_wm <- data_box_plot %>% filter(Genotype!="HET" & Day != "Testing")
 t.test(Percent_Time_Investigated~Genotype,data_wm)
 wilcox.test(Percent_Time_Investigated~Genotype,data_wm)
+#0.6083, 0.4742
 
 # WT vs MUT Testing
-data_wm <- data_box_plot %>% filter(Genotype!="HET", Day != "Training")
+data_m <- data_box_plot %>% filter(Genotype!="HET")
+data_wm <- data_m %>% filter(Day != "Training")
 t.test(Percent_Time_Investigated~Genotype,data_wm)
 wilcox.test(Percent_Time_Investigated~Genotype,data_wm)
+#0.6008, 0.5744
 
 # WT vs HET: Training
-data_wh <- data_box_plot %>% filter(Genotype!="MUT", Day != "Testing")
+data_wh <- data_box_plot %>% filter(Genotype!="MUT" & Day != "Testing")
 t.test(Percent_Time_Investigated~Genotype,data_wh, exact = FALSE)
 wilcox.test(Percent_Time_Investigated~Genotype,data_wh, exact = FALSE)
-
+#0.4504, 0.2475
 
 # WT vs HET: Testing
-data_wh <- data_box_plot %>% filter(Genotype!="MUT", Day != "Training")
+data_wh <- data_box_plot %>% filter(Genotype!="MUT" & Day != "Training")
 t.test(Percent_Time_Investigated~Genotype,data_wh, exact = FALSE)
 wilcox.test(Percent_Time_Investigated~Genotype,data_wh, exact = FALSE)
+#0.1355, 0.1979
 
 #Longitudinal Slope Test
 Percent_Investigation_Time = data_box_plot$Percent_Time_Investigated
