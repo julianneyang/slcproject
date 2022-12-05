041c9516fe0cb9d427549e72eca123154e3d542b
田丰睿 <tianfengrui@tianfengruideMacBook-Pro.local>
2022-12-05 14:20:26 -0800

    ASO behavior

---
+++ ASO/ASO_Rproj/Food_Pellet.R
@@ -0,0 +1,44 @@
+
+library(ggplot2)
+library(rlang)
+library(cowplot)
+library(ggpubr)
+library(viridis)
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+
+setwd("~/Documents/slcspontaneous/ASO/Analysis_Files/")
+data<-read.csv("ASO_Food_Pellet_Test_Data .csv", header=TRUE)
+
+generate_boxplots <- function(input_data, X, Y, min,max){
+  data<-as.data.frame(input_data)
+  #Ensure correct ordering of levels 
+  data$Genotype <- factor(data$ASO_Tg, levels = c("Negative", "Positive"))
+  
+  ggplot(data=data,aes(x={{X}},y={{Y}}, fill={{X}})) + 
+    #geom_violin(alpha=0.25,position=position_dodge(width=.75),size=1,color="black",draw_quantiles=c(0.5))+
+    geom_boxplot(alpha=0.25)+
+    scale_fill_viridis_d()+
+    geom_point(size=1,position=position_jitter(width=0.25),alpha=0.1)+
+    theme_cowplot(16) +
+    #ylim(min,max)+
+    #facet_grid(~Sex)+    
+    theme(legend.position = "none")
+  
+}
+
+# Total_Time vs ASO_Tg
+
+a<-generate_boxplots(data, ASO_Tg, Total_Time,0,900)+
+  stat_compare_means(comparisons = list(c("Negative", "Positive") 
+                                      ),method="wilcox", vjust=0.3,label="p.signif",step.increase=0.05)
+a
+
+#parametric and non-parametric stats
+#Total_Time
+
+data<-read.csv("ASO_Food_Pellet_Test_Data .csv", header=TRUE)
+t.test(Total_Time~ASO_Tg,data)
+wilcox.test(Total_Time~ASO_Tg,data)
+
---
+++ ASO/ASO_stats/ASO_stats.md
@@ -0,0 +1,17 @@
+Welch Two Sample t-test
+
+data:  Total_Time by ASO_Tg
+t = 0.30437, df = 31.588, p-value = 0.7628
+alternative hypothesis: true difference in means between group Negative 
+and group Positive is not equal to 0
+95 percent confidence interval:
+ -115.2326  155.6948
+sample estimates:
+mean in group Negative mean in group Positive 
+              497.0072               476.7761
+
+Wilcoxon rank sum test with continuity correction
+
+data:  Total_Time by ASO_Tg
+W = 182, p-value = 0.5372
+alternative hypothesis: true location shift is not equal to 0
